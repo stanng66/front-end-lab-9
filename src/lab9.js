05928712663badbd775b5ebc6c63e12950a894c8
Stanley Nguyen <stan.ng66@gmail.com>
2026-07-28 10:45:35 -0400

    Improve styling and formatting

--- src/lab9.js
+++ src/lab9.js
@@ -5,6 +5,7 @@
 // Ronak Sheth
 // August 4, 2026
 // ====================
+import "./lab9.css"
 import { useEffect, useState } from "react";
 
 export default function Lab9() {
@@ -22,20 +23,42 @@ export default function Lab9() {
   }, []);
 
   return (
-    <div>
-      <h1>Currency List</h1>
-      <h2>Stanley Nguyen</h2>
-      <h3>Lab 9</h3>
+    <> 
+      <div className="header">
+        <h1>Currency List</h1>
+        <h2>Stanley Nguyen</h2>
+        <h3>Lab 9</h3>
+      </div>
 
-      <p style={{ color: "red" }}>{errors}</p>
+      <div className="currency-container">
+        <p style={{ color: "red" }}>{errors}</p>
 
-      {currencies.map((item) => (
-        <div key={item.id}>
-          <p><strong>Currency Code:</strong> {item.code}</p>
-          <p><strong>Currency Name:</strong> {item.currency}</p>
-          <hr />
-        </div>
-      ))}
-    </div>
+        {/* {currencies.map((item) => (
+          <div className="currency-info" key={item.id}>
+            <p><strong>Currency Code:</strong> {item.code}</p>
+            <p><strong>Currency Name:</strong> {item.currency}</p>
+            <hr />
+          </div> 
+        ))} */}
+
+        <table className="currency-table">
+          <thead>
+            <tr>
+              <th>Currency Code</th>
+              <th>Currency Name</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {currencies.map((item) => (
+              <tr key={item.id}>
+                <td>{item.code}</td>
+                <td>{item.currency}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
